d03fb549ea74c2693458e07bbdd73b02e0ef4e53
Mike Crowe <drmikecrowe@gmail.com>
2023-07-01 09:43:17 -0400

    WIP changes

--- home-manager/apps/console/bash/default.nix
+++ home-manager/apps/console/bash/default.nix
@@ -3,7 +3,6 @@
 {
   programs.bash = {
     enableCompletion = true;
-    keychain.enableBashIntegration = true;
   };
 
   home.packages = with pkgs; [ exa neofetch starship ];
--- home-manager/apps/console/nushell/default.nix
+++ home-manager/apps/console/nushell/default.nix
@@ -3,7 +3,6 @@
 {
   programs.nushell = {
     enableCompletion = true;
-    keychain.enableNushellIntegration = true;
   };
 
   home.packages = with pkgs; [ exa neofetch starship ];
