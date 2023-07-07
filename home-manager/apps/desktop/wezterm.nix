0fc114fc2599c2d052c989d72ca4b14f7f335c13
Mike Crowe <drmikecrowe@gmail.com>
2023-07-07 06:29:57 -0400

    Fixing wezterm colors

--- home-manager/apps/desktop/wezterm.nix
+++ home-manager/apps/desktop/wezterm.nix
@@ -6,25 +6,12 @@
   ];
 
   xdg.configFile."wezterm/wezterm.lua".text = ''
-    local wezterm = require 'wezterm'
-
-    local my_framer = wezterm.color.get_builtin_schemes()['Framer (base16)']
-    my_framer.cursor_fg = '#181818'
-    my_framer.cursor_bg = '#EEEEEE'
-    my_framer.compose_cursor = '#20BCFC'
-    
     return {
       font_size = 14,
       window_background_opacity = 1,
       hide_tab_bar_if_only_one_tab = true,
       default_cursor_style = "BlinkingUnderline",
       cursor_blink_rate = 600,
-
-      color_schemes = {
-        ['My Framer'] = my_framer,
-      },
-
-      color_scheme = 'My Framer',
     }
   '';
 }
