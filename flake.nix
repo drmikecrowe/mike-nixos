8236ff25ebe5915eefc0a7ab71db4726906cf982
Mike Crowe <drmikecrowe@gmail.com>
2023-07-08 22:28:33 -0400

    back to nvidia-only

--- flake.nix
+++ flake.nix
@@ -69,7 +69,7 @@
           modules = [
             # > Our main nixos configuration file <
             ./hosts/xps15/configuration.nix
-            nixos-hardware-test.outputs.nixosModules.dell-xps-15-9560
+            nixos-hardware-test.outputs.nixosModules.dell-xps-15-9560-nvidia
             home-manager.nixosModules.home-manager
             {
               home-manager = {
