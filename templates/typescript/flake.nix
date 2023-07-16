c69525c4f2b892aa9785ea4628a14126a8538f2f
Mike Crowe <drmikecrowe@gmail.com>
2023-07-16 10:13:51 -0400

    Fixing typescript template

--- templates/typescript/flake.nix
+++ templates/typescript/flake.nix
@@ -1,29 +1,21 @@
 {
   description = "Typescript project";
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-  outputs = { nixpkgs, mach-nix }:
-    let
-      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-darwin" ];
-      forAllSystems = f:
-        nixpkgs.lib.genAttrs supportedSystems
-          (system: f system (import nixpkgs { inherit system; }));
-    in
-    rec {
-      devShell = forAllSystems (system: pkgs:
-        pkgs.mkShell {
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
           buildInputs = [
-            # pkgs.nodejs
-            # You can set the major version of Node.js to a specific one instead
-            # of the default version
             pkgs.nodejs-18_x
-
-            # You can choose pnpm, yarn, or none (npm).
             pkgs.nodePackages.pnpm
-            # pkgs.yarn
-
             pkgs.nodePackages.typescript
             pkgs.nodePackages.typescript-language-server
           ];
-        });
-    };
+        };
+      });
 }
