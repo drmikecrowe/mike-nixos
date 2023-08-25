3ec46ce580a9dbf637b7a9f9f6a94fdf201d3bbc
Mike Crowe <drmikecrowe@gmail.com>
2023-08-24 20:04:05 -0400

    "Added new feature to improve user experience"

--- modules/common/applications/kitty.nix
+++ modules/common/applications/kitty.nix
@@ -35,6 +35,7 @@
           "kitty_mod+backspace" = "change_font_size current 0";
           "kitty_mod+c" = "copy_to_clipboard";
           "kitty_mod+v" = "paste_from_clipboard";
+          "kitty_mod+n" = "launch --type=tab kitty nu";
           "ctrl+k" = "clear_terminal reset active";
         };
 
