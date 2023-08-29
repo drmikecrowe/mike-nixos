a2d8382b83d066d2a173b36683de2f6146b3b6ae
Mike Crowe <drmikecrowe@gmail.com>
2023-08-29 06:59:35 -0400

    "Fix bug in login functionality"

--- modules/common/programming/continue-server.nix
+++ modules/common/programming/continue-server.nix
@@ -9,7 +9,6 @@
       after = [ "network.target" ];
       serviceConfig = {
         Type = "simple";
-        User = "${config.user}";
         WorkingDirectory = "/home/${config.user}/Programming/Personal/continue";
         Environment = "PATH=/home/${config.user}/.nix-profile/bin:/run/wrappers/bin:/run/current-system/sw/bin";
         ExecStart = "${pkgs.bash}/bin/bash -c '${pkgs.direnv}/bin/direnv exec . python3 -m continuedev.src.continuedev.server.main'";
