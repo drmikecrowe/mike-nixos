b948bfd8fb076327b962e786ec8af429df616311
Mike Crowe <drmikecrowe@gmail.com>
2023-09-19 05:42:11 -0400

    "Update README.md with new instructions"

--- modules/common/programming/vscode-fhs.nix
+++ modules/common/programming/vscode-fhs.nix
@@ -1,11 +1,11 @@
 { config, pkgs, lib, ... }: {
 
-  environment.systemPackages = with pkgs; [ vscode.fhs ];
+  # environment.systemPackages = with pkgs; [ vscode.fhs ];
 
-  xdg.portal = {
-    enable = true;
-    wlr.enable = true;
-    extraPortals = with pkgs; [ xdg-desktop-portal-gtk ];
-    #xdgOpenUsePortal = true;
-  };
+  # xdg.portal = {
+  #   enable = true;
+  #   wlr.enable = true;
+  #   extraPortals = with pkgs; [ xdg-desktop-portal-gtk ];
+  #   #xdgOpenUsePortal = true;
+  # };
 }
